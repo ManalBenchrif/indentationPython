3ac0842557907ef1577fc76ea2b3e59fab80aa17
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-19 23:07:13 +0100

    Add files via upload

---
+++ indentation.py
@@ -0,0 +1,9 @@
+#for blocks python use indentation instead of curly braces.
+#both spaces and tabs are supported ,but try to not mix them in same block (some time it gives bugs).
+#the standar identation is to use 4 spaces
+
+x=10
+if x==10:
+    #4 spaces
+    print("value of x =",x)
+print("end of if block")
